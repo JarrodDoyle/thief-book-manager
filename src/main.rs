f5e12466ac299b4a813486bc714e4194eb915915
Jarrod Doyle <thejarroddoyle@gmail.com>
2024-01-21 15:41:41 +0000

    Make basic iced window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{widget::text, Element, Result, Sandbox, Settings};
+
+#[derive(Debug)]
+enum Message {}
+
+struct Application;
+
+impl Sandbox for Application {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self
+    }
+
+    fn title(&self) -> String {
+        String::from("Thief Book Manager")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {}
+    }
+
+    fn view(&self) -> Element<'_, Message> {
+        text("Hello, world!").into()
+    }
+}
+
+fn main() -> Result {
+    Application::run(Settings::default())
 }
